733bdc9d87190e872a907c7c4995158824d287e3
Christopher Bogs <cbogs@upenn.edu>
2023-11-18 14:48:55 -0500

    Setup

---
+++ rockpaperscissors.js
@@ -0,0 +1,16 @@
+/*
+LOOP
+CHOOSE rock, paper, or scissors randomly for the computer player
+GET player choice of rock, paper or scissors
+    REGULARIZE input
+    CHECK whether input matches rock, paper, scissors, or quit (or r, p, s, q)
+    IF input is Q then exit, ELSE continue
+EVALUATE which player wins
+    paper beats rock
+    rock beats scissors
+    scissors beats paper
+    ties are ties
+REPORT winner
+REPEAT
+*/
+
