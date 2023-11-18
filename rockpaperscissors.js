35b28764a1c00e51963202e5fe94b9960a598f7a
Christopher Bogs <cbogs@upenn.edu>
2023-11-18 15:00:12 -0500

    implement getComputerChoice(), chooseWeapon()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -14,3 +14,35 @@ REPORT winner
 REPEAT
 */
 
+getComputerChoice();
+console.log(computerChoice);
+
+
+function getComputerChoice() {
+
+    //CHOOSE rock, paper, or scissors randomly for the computer player
+    // Do I want to use numbers or strings? Probably strings are easiest
+
+    let computerChoice = chooseWeapon();
+    return computerChoice;   
+
+}
+
+
+function chooseWeapon() {
+
+    let weaponNum = (Math.floor(Math.random() * 3)); // 0 Rock, 1 Paper, 2 Scissors
+    switch weaponNum {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+        default:
+            return "RANDOMIZATION ERROR";
+    }
+}
